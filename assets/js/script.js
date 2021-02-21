7a5d24b7a071e3829804c34eb6eacd23162e1cb0
JPB <jbanas9124@gmail.com>
2021-02-21 17:40:19 -0600

    Creating number of characters option for password, lowercase prompt, uppercase prompt, and special character prompt

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,44 @@
 // Assignment code here
+//create a function to prompt for criteria
+var characterNumber = function() {
+  var num = "";
+  while (num >= 8 || num <= 128){
+    num = prompt("Choose a number of characters between 8 - 128.");
+  }
+  console.log(`"You have chosen "` + num + " of characters.");
+  return num;
+}
+
+var lowerCasePrompt = function() {
+  var confirmLowercase = window.confirm("Would you like lowercase characters?");
+  if (confirmLowercase) {
+    window.alert("You have added lowercase letters.");
+  }
+}
+
+var upperCasePrompt = function() {
+  var confirmLowercase = window.confirm("Would you like uppercase characters?");
+  if (confirmLowercase) {
+    window.alert("You have added uppercase letters.");
+  }
+}
+
+var numberPrompt = function() {
+  var confirmLowercase = window.confirm("Would you like to add numbered characters?");
+  if (confirmLowercase) {
+    window.alert("You have added numbered letters.");
+  }
+}
+
+
+
+var lower = charset = 'abcdefghijklmnopqrstuvwxyz';
+
+var upper = charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+var number = charset = '0123456789';
+
+var specialCharacter = charset =` !"#$%&'()*+,-./:;<=>?@[\]^_{|}~"`
 
 
 // Get references to the #generate element
@@ -26,9 +66,13 @@ generateBtn.addEventListener("click", writePassword);
       1) at least 8 characters, no more than 128
     b) when prompted for character type, user can select(include one or multiple):
       1)lowercase
+      'abcdefghijklmnopqrstuvwxyz'
       2)uppercase
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
       3)numeric
+      '0123456789'
       4)special characters
+      INCLUDE SPACEBAR  " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
   
   4) After answering each prompt input should be validated and at least one input type should be selected
   5)All the prompts are given a valid input
