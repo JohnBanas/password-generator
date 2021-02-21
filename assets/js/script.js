801b66680e798451ee8674b868557e7b552f315c
JPB <jbanas9124@gmail.com>
2021-02-21 15:47:44 -0600

    Psuedo code

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,30 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+/* Create new secure password:
+ 1) When button is clicked it prompts the user with a series of criteria
+ 2) When prompted for criteria, user selects which options to include in password creation
+ 3) criteria includes:  
+  
+    a) Length of password
+      1) at least 8 characters, no more than 128
+    b) when prompted for character type, user can select(include one or multiple):
+      1)lowercase
+      2)uppercase
+      3)numeric
+      4)special characters
+  
+  4) After answering each prompt input should be validated and at least one input type should be selected
+  5)All the prompts are given a valid input
+  6)Password generated to match selected criteria
+  7)Password should be written in an alert or written to the page.*/
+  
+  
+
+
+
+
+
+
