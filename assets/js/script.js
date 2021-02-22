195706869516ea7ddd955252c5a1564599071704
JPB <jbanas9124@gmail.com>
2021-02-22 16:50:25 -0600

    Changed query variables to string arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 // Assignment code here
 
 //variables for criteria
-var lower = charset = 'abcdefghijklmnopqrstuvwxyz';
+var caseLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-var upper = charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var caseUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
-var number = charset = '0123456789';
+var number = ["0","1","2","3","4","5","6","7","8","9"];
 
-var specialCharacter = charset =` !"#$%&'()*+,-./:;<=>?@[\]^_{|}~"`
+var specChar = ["!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","{","|","}","~",'\\'];
 
 //function to generate password
 function generatePassword() {
