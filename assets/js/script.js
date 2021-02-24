efd9b1033cf022725654e6846fdb970780651e09
JPB <jbanas9124@gmail.com>
2021-02-24 13:10:29 -0600

    queries functioning and adding to individual arrays to mainArray. function now working for length of password as well.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 // Assignment code here
 
 //variables for criteria
+
+
 var caseLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 var caseUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
@@ -9,52 +11,84 @@ var number = ["0","1","2","3","4","5","6","7","8","9"];
 
 var specChar = ["!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","{","|","}","~",'\\'];
 
+var mainArray = [];
+
+
+
+//number of characters in password function
+function passLength() {
+  num = prompt("Choose a number of characters for your password between 8 - 128.");
+  if (num < 8 || num > 128 || isNaN(num) === true) {
+    window.alert("Please choose between 8 - 128.");
+    passLength();
+  }
+}
+ 
+
+
 //function to generate password
-function generatePassword() {
-//create a function to prompt for criteria
-var charNumber = function() {
-  var num = "";
-  while (num < 8 && num > 128 ){
-    num = prompt("Choose a number of characters between 8 - 128.");
-  }
-  if (num >= 8 || num <= 128) {
-  console.log("You have chosen " + num + " of characters.");
-  }
-  return charNumber(num);
-  }
+var generatePassword = function() {
+ //create a function to prompt for criteria
 
-  var loCasePrompt = function() {
-  var confirmLowercase = prompt("Would you like lowercase characters?");
+ //call the number of the password
+ passLength();
+
+  //confirm lowercase
+
+  var confirmLowercase = window.confirm("Would you like lowercase characters, click 'OK' for YES or click 'CANCEL' for NO.");
+
   if (confirmLowercase) {
-    alert("You have added lowercase characters.");
+    mainArray.push(caseLower);
+    for (i = 0; i < caseLower.length; i ++){
+      mainArray.push(caseLower[i]);
+    }
   }
-  (loCasePrompt(lower));
+  console.log(mainArray.length);
+  //confirm uppercase
+
+  var confirmUppercase = window.confirm("Would you like uppercase characters, click 'OK' for YES or click 'CANCEL' for NO.");
+
+  if (confirmUppercase) {
+    mainArray.push(caseUpper);
+    for (i = 0; i < caseUpper.length; i ++){
+      mainArray.push(caseUpper[i]);
+    }
+  }
+  console.log(mainArray.length);
+  
+  //confirm numbers
+
+  var confirmNum = confirm("Would you like to add numbered characters, click 'OK' for YES or click 'CANCEL' for NO.");
+
+  if (confirmNum) {
+    mainArray.push(number);
+    for (i = 0; i < number.length; i ++){
+      mainArray.push(number[i]);
+    }
   }
 
-  var upCasePrompt = function() {
-  var confirmLowercase = prompt("Would you like uppercase characters?");
-  if (confirmLowercase) {
-    alert("You have added uppercase characters.");
-  }
-  upCasePrompt(upper);
+  console.log(mainArray.length);
+  
+  //confirm special characters
+
+  var confirmSpecialCharacter = window.confirm("Would you like to add special characters, click 'OK' for YES or click 'CANCEL' for NO.");
+
+  if (confirmSpecialCharacter) { 
+    mainArray.push(specChar);
+    for (i = 0; i < specChar.length; i ++){
+      mainArray.push(specChar[i]);
+    }
   }
 
-  var numPrompt = function() {
-  var confirmLowercase = prompt("Would you like to add numbered characters?");
-  if (confirmLowercase) {
-    alert("You have added numbered characters.");
-  }
-  numPrompt(number);
-  }
+  console.log(mainArray.length);
 
-  var specCharacter = function() {
-  var confirmSpecialCharacter = prompt("Would you like to add special characters?");
-  if (confirmSpecialCharacter) {
-    alert("You have added special characters.");
-  }
-  specCharacter(specialCharacter);
-  }
-};
+  
+  
+  //end of queries.
+  // function to use number of characters and call generatePassword
+  
+};    
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -92,7 +126,25 @@ generateBtn.addEventListener("click", writePassword);
   4) After answering each prompt input should be validated and at least one input type should be selected
   5) All the prompts are given a valid input
   6) Password generated to match selected criteria
-  7) Password should be written in an alert or written to the page.*/
+  7) Password should be written in an alert or written to the page.
+  
+  
+    if (num > 8 && num < 128) {
+    console.log("Number of characters in password." + num);
+    } else {
+
+  }
+
+
+var finalPassword =function() {
+    for (var i=0; i < parseInt(passLength); i++) {
+      mainArray[Math.floor(mainArray.length * Math.random())];
+    }
+  }  
+
+  console.log(finalPassword.toString());
+  return('"' + finalPassword + '"' + ' is your new password.');
+  */
   
   
 
