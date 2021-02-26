1e9d6078e40c96b647d19538ae7a64b286408c5a
JPB <jbanas9124@gmail.com>
2021-02-25 23:52:18 -0600

    Had to create a function to reset values on click. Now all requirements met.

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,17 +14,22 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+generateBtn.addEventListener("click", resetValues);
 
+function resetValues() {
+  mainArray = [];
+  passwordLength = [];
+}
 
 //variables for criteria
 
 var caseLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-var caseUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var caseUpper = ["A","B","C","D","E","F","G","H","I","J","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
-var number = ["0","1","2","3","4","5","6","7","8","9"];
+var number = ["1","2","3","4","5","6","7","8","9"];
 
-var specChar = ["!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","{","|","}","~",'\\'];
+var specChar = [`!`,`"`,`#`,`$`,`%`,`&`,`'`,`(`,`)`,`*`,`-`,`_`,`=`,`+`,`~`,`@`,`^`,`;`,`:`,`/`,`?`,`|`,`\\`,`,`,`.`,`<`,`>`,"`"];
 
 var mainArray = [];
 
@@ -86,7 +91,7 @@ var allArrayContainer = function() {
 var generatePassword = function() {
 
   //a container for the random password array
-  let prePassword = [];
+  let prePassword=[];
   
 
   //call the number of the password
